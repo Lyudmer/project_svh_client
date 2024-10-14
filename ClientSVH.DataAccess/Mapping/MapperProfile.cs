230e806ae9ab2bc48a3f36a799c83d058040474b
e.lyudmer <e.lyudmer@ctm.ru>
2024-10-14 19:41:00 +0300

    add mapping

---
+++ ClientSVH.DataAccess/Mapping/MapperProfile.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using ClientSVH.Core.Models;
+using ClientSVH.DataAccess.Entities;
+
+namespace ClientSVH.DataAccess.Mapping
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<DocumentEntity, Document>()
+                .ForAllMembers(x => x.Condition((src, dest, prop) =>
+                {
+                    if (prop == null) return false;
+                    if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+                    return true;
+                }
+                ));
+            CreateMap<PackageEntity, Package>()
+                .ForAllMembers(x => x.Condition((src, dest, prop) =>
+                {
+                    if (prop == null) return false;
+                    if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+                    return true;
+                }
+                ));
+            CreateMap<StatusEntity, Status>()
+                .ForAllMembers(x => x.Condition((src, dest, prop) =>
+                {
+                    if (prop == null) return false;
+                    if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+                    return true;
+                }
+                ));
+            CreateMap<UserEntity, User>()
+                .ForAllMembers(x => x.Condition((src, dest, prop) =>
+                {
+                    if (prop == null) return false;
+                    if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+                    return true;
+                }
+                ));  
+            CreateMap<HistoryPkgEntity, HistoryPkg>()
+                .ForAllMembers(x => x.Condition((src, dest, prop) =>
+                {
+                    if (prop == null) return false;
+                    if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+                    return true;
+                }
+                ));
+        }
+    }
+}
