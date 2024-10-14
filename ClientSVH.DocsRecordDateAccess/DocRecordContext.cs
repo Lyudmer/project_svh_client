aae7194c51901205c1c62358a41d5f70c163abad
Lyudmer <ludmerekaterina@gmail.com>
2024-10-14 21:33:42 +0300

    fixed error

---
+++ ClientSVH.DocsRecordDateAccess/DocRecordContext.cs
@@ -0,0 +1,28 @@
+﻿
+using ClientSVH.DocsRecordCore.Abstraction;
+using ClientSVH.DocsRecordCore.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ClientSVH.DocsRecordDataAccess
+{
+    public class DocRecordContext : IDocRecordContext
+    {
+        private readonly IMongoDatabase _database = null;
+        public DocRecordContext(IOptions<Settings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            if (client != null)
+            {
+                _database = client.GetDatabase(settings.Value.Database);
+            }
+        }
+        public IMongoCollection<DocRecord> DocRecords
+        {
+            get
+            {
+                return _database.GetCollection<DocRecord>("DocRecord");
+            }
+        }
+    }
+}
