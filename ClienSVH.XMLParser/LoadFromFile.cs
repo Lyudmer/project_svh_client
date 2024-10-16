6736902bd8dae11f9c1adca923b65a54b8c8dee8
e.lyudmer <e.lyudmer@ctm.ru>
2024-10-16 11:41:01 +0300

    fixed error LoadFileXml and setting mongodb

--- ClienSVH.XMLParser/LoadFromFile.cs
+++ ClienSVH.XMLParser/LoadFromFile.cs
@@ -5,10 +5,12 @@ using ClientSVH.Core.Abstraction.Repositories;
 using ClientSVH.Core.Models;
 using ClientSVH.DocsRecordCore.Abstraction;
 using ClientSVH.DocsRecordCore.Models;
+using System;
 using System.Data;
 
 
 using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 namespace ClienSVH.XMLParser
@@ -27,43 +29,38 @@ namespace ClienSVH.XMLParser
                 XDocument xFile = XDocument.Parse(inFile.Trim());
 
                 var xPkg = xFile.Element("Package")?
-                    .Elements().Where(p => p.FirstAttribute?.Name.LocalName == "CfgName");
-                
+                    .Elements().Where(p => p.Attributes().Where(a => a.Name.LocalName.Contains("CfgName")).FirstOrDefault()?.Value is not null);
+
                 if (xPkg is not null)
                 {
-                    
+                    var cDoc = xPkg.Count(); 
                     //create package
                     var Pkg = Package.Create(Pid, userId, 0, Guid.NewGuid(), DateTime.UtcNow, DateTime.UtcNow);
                     Pkg = await _pkgRepository.Add(Pkg);
                     Pid = Pkg.Id;
-
-                    var xDocs = from xDoc in xPkg?.AsParallel()
-                                select new
-                                {
-                                    tdoc = xDoc.Name?.LocalName,
-                                    num = xDoc.Elements().Elements().FirstOrDefault(n => n.Name == "RegNum")?.Value,
-                                    dat = xDoc.Elements().Elements().FirstOrDefault(n => n.Name == "RegDate")?.Value,
-                                    doctext = xDoc.ToString()
-
-                                };
-                    foreach (var doc in xDocs)
+                   
+                    foreach (var xDoc in xPkg)
                     {
                         var LastDocId=_docRepository.GetLastDocId().Result+1;
-                        string docCode = doc.tdoc.Contains("CONOSAMENT") ? "02011" : "09999";
+                        var tdoc = xDoc.Name.LocalName;
+                        var num = xDoc.Elements().Elements("RegNum").FirstOrDefault()?.Value.ToString();
+                        var dat = xDoc.Elements().Elements("RegDate").FirstOrDefault()?.Value.ToString();
+                        var doctext = xDoc.ToString();
+                        string docCode = tdoc.Contains("CONOSAMENT") ? "02011" : "09999";
                         DateTime DocDate = DateTime.UtcNow;
-                        if (doc.dat is not null)
-                            _ = DateTime.TryParse(doc.dat, out DocDate);
+                        if (dat is not null)
+                            _ = DateTime.TryParse(dat, out DocDate);
 
-                        var Doc = Document.Create(LastDocId, Guid.NewGuid(), doc.num, DocDate, docCode,
-                                      doc.tdoc, doc.doctext.Length, DopFunction.GetHashMd5(doc.doctext),
-                                      DopFunction.GetSha256(doc.doctext),
+                        var Doc = Document.Create(LastDocId, Guid.NewGuid(), num, DocDate, docCode,
+                                      tdoc, doctext.Length, DopFunction.GetHashMd5(doctext),
+                                      DopFunction.GetSha256(doctext),
                                       Pid, DateTime.UtcNow, DateTime.UtcNow);
 
                         
                         Doc = await _docRepository.Add(Doc);
                         if (Doc is not null)
                         {
-                            DocRecord dRecord = DocRecord.Create(Doc.DocId, doc.doctext);
+                            DocRecord dRecord = DocRecord.Create(Doc.DocId, doctext);
                             var dRecordId = await _docRecordRepository.AddRecord(dRecord);
                         }
                     }
